55be34710a9fec769675ee8bf57d9e091c5b7cf2
andela-oadeniran <oladipupo.adeniran@andela.com>
2018-02-20 18:32:19 +0100

    I hope this is it

--- README.md
+++ README.md
@@ -11,4 +11,4 @@
 4. Create a virtualenv ` mkvirtualenv koda --python=/path/to/python3`.
 5. Install the requirements `pip install -r requirements.txt`
 6. Navigate to the koda directory and the entry is the pizza.py file
-7. Run `python pizza.py` to test what we currently have.
+7. Run `python pizza.py example.in` to test what we currently have.
--- pizza.py
+++ pizza.py
@@ -3,116 +3,97 @@
 2. MVP
 '''
 import numpy as np
+import sys
 
 
-rows = 3
-columns = 5
-minimum = 1
-maximum = 6
-
-
-row1 = ['T', 'T', 'T', 'T', 'T']
-row2 = ['T', 'M', 'M', 'M', 'T']
-row3 = ['T', 'T', 'T', 'T', 'T']
-
-
-pizza = np.array([row1, row2, row3])
-
-# recursion
-# def count_occurrence_of(arr):
-#     unique, counts = np.unique(arr, return_counts=True)
-#     least_spice, least_spice_total = sorted(list(zip(unique,counts)), key=lambda pair: pair[1])[0]
-#     spices_and_count = dict(tuple(zip(unique, counts)))
-#
-#     max_possible_valid_slices = least_spice_total // minimum
-#     # first minimum occurrence of the least_spice
-#     # position
-#     start_positions = [[0, 0]]
-#     end_positions = []
-#     minimum_spice = []
-#     while (len(minimum_spice) < minimum):
-#
-#         for spice in np.nditer(arr[start_positions[-1]]):
-#             if (spice == least_spice):
-#                 minimum_spice.append(spice)
-#
-#
-# def find_positions(np_arr, least_spice, start_positions=[(0,0)], end_positions=[]):
-#     end_position = list(end_positions[-1])
-#     if end_positions and (np_arr[list(end_positions[-1][0]), list(end_positions[-1][1])] == ):
-#         return start_positions, end_positions
-#     minimum_spice = []
-#     while (len(minimum_spice) < minimum):
-#         start_position = list(start_positions[-1])
-#         for spice in np.nditer(np_arr[start_position[0], start_position[1]]):
-#             if (spice == least_spice):
-#                 minimum_spice.append(spice)
-#
-#
-#
-# def find_positions_2(np_arr, least_spice):
-#     match = False
-#     x, y = 0, 0
-#     test_x, test_y = minimum, 0
-#     test_start = slice(test_x, test_y)
-#     unique, counts = np.unique(np_arr[test_start], return_counts=True)
-#     spices_and_count = dict(tuple(zip(unique, counts)))
-#     while not match:
-#         if spices_and_count.get(least_spice) == minimum:
-#             match = True
-
-
-def main(np_arr):
-    # np_arr = np.transpose(np_arr)
-    # print(np_arr)
+def calculate_pizza_slices(np_arr,rows, columns, minimum, maximum):
+    positions = []
     start_positions = []
     end_positions = []
     start_x = 0
     start_y = 0
-    end_x = 0
-    end_y = 0
-    for index, spice_position in enumerate(np.nditer(np_arr)):
-        print(np_arr[start_x:end_x, start_y:end_y])
-
-        # print(np_arr[0:3, 0:3])
-        print('------------------------------')
-        # print(index)
-        # pizza_slice = slice()
-        # minimum_for_each_slice, maximum_for_both_slices = calculate_requirements(len(start_positions))
-        # if check_array_slice_for_minimum_condition(pizza_slice, minimum_for_each_slice, maximum_for_both_slices):
-        #     pass
-        # print(check_array_slice_for_minimum_condition(np_arr[start_x:end_x, start_y:end_y], minimum, maximum))
+    end_x = 1
+    end_y = 1
+    stop = False
+    # for index, spice_position in enumerate(np.nditer(np_arr)):
+    while not stop:
+        # if index < 10:
+        #     print('-' * 20)
+        #     print(start_x)
+        #     print (start_y)
+        #     print(end_x)
+        #     print(end_y)
+        #     print('-' * 20)
         if check_array_slice_for_minimum_condition(np_arr[start_x:end_x, start_y:end_y], minimum, maximum):
-            start_positions.extend([start_x, end_x - 1])
-            end_positions.extend([start_y, end_y - 1])
+            # start_positions.extend([start_x, end_x - 1])
+            # end_positions.extend([start_y, end_y - 1])
+            positions.append((start_x, start_y, end_x - 1, end_y - 1))
+            # Determine the starting position
             start_y = end_y
-            end_x = start_x + minimum
-            end_y = start_y + minimum
-            # print("x", end_x)
-            # print("y", end_y)
+            end_y += 1
         # check if the slice is too big
-        elif np_arr[start_x:end_x, start_y:end_y].size >= maximum:
+        elif np_arr[start_x:end_x, start_y:end_y].size > maximum:
             # start_x += 1
-            if start_x > start_y:
+            # start_x, start_y = x_y_axis(start_x, start_y, True)
+            if start_x < rows:
+                if (start_x <= start_y) or (start_y == columns - 1):
+                    start_x += 1
+                else:
+                    start_y += 1
+            elif start_y < columns:
                 start_y += 1
             else:
-                start_x += 1
+                stop = True
         else:
-            if end_x > end_y:
+            # end_x, end_y = x_y_axis(end_x, end_y)
+            if (end_x <= rows):
+                if (end_x <= end_y) or (end_y > columns):
+                    end_x += 1
+                elif end_x > end_y:
+                    end_x -= 1
+                    end_y += 1
+                else:
+                    end_x += 1
+            elif end_y <= columns:
                 end_y += 1
             else:
-                end_x += 1
+                stop =  True
+            # if (end_x == end_y) and (end_x <= rows):
+            #     end_x += 1
+            # elif (end_x > end_y) and (end_y <= columns):
+            #     end_x -= 1
+            #     end_y += 1
+            # else:
+            #     end_x += 1
+    # result = zip(start_positions, end_positions)
+    # print(list(result))
+    # print(len(positions))
+    # print(positions)
+    return (len(positions), positions)
 
 
-    result = zip(start_positions, end_positions)
-    print(list(result))
+def write_result_to_file(number_of_slices, positions, filename):
+    with open(filename, 'w') as f:
+        f.write(str(number_of_slices) + '\n')
+        print(number_of_slices)
+        for slice_axis in positions:
+            f.write(' '.join(( str(char) for char in slice_axis )) + '\n')
+            print(' '.join(( str(char) for char in slice_axis )))
 
-
-def calculate_requirements(number_of_slices_got):
-    minimum_for_each_slice = number_of_slices_got * minimum
-    maximum_for_both_slices = number_of_slices_got * maximum
-    result = (minimum_for_each_slice, maximum_for_both_slices)
-    return result
+# def x_y_axis(x, y, axis_start=False):
+#     if x == y:
+#         x+=1
+#         return (x, y)
+#     elif axis_start:
+#         y += 1
+#         return (x, y)
+#     elif x > y:
+#         y+=1
+#         x-=1
+#         return(x, y)
+#     else:
+#         x+=1
+#         return(x, y)
 
 
 def check_array_slice_for_minimum_condition(pizza_slice, minimum_for_each_slice, maximum_for_both_slices):
@@ -120,13 +101,31 @@ def check_array_slice_for_minimum_condition(pizza_slice, minimum_for_each_slice,
     spices_and_count = dict(zip(unique, counts))
     tomato_count = spices_and_count.get('T', None)
     mushroom_count = spices_and_count.get('M', None)
-    # print(tomato_count)
-    # print(mushroom_count)
-    # import pdb;pdb.set_trace()
+
     if not (mushroom_count and tomato_count):
         return False
     pizza_slice_count = tomato_count + mushroom_count
-    slice_is_satisfied = (tomato_count >= minimum_for_each_slice) and (mushroom_count >= minimum_for_each_slice) and (pizza_slice_count < maximum_for_both_slices)
+    slice_is_satisfied = (tomato_count >= minimum_for_each_slice) and (mushroom_count >= minimum_for_each_slice) and (pizza_slice_count <= maximum_for_both_slices)
     return slice_is_satisfied
 
-main(pizza)
+def word_to_list(wrd):
+    return [ char for char in wrd ][:-1]
+
+def main():
+    np_array = []
+    data_file = sys.argv[1]
+    with open(data_file) as f:
+        for index, line in enumerate(f):
+            if index == 0:
+                # details = line.split()
+                rows, columns, minimum, maximum = [ int(char) for char in line.split()  ]
+            else:
+                np_array.append(word_to_list(line))
+    np_array = np.array(np_array)
+    # calculate_pizza_slices(np_array,rows, columns, minimum, maximum)
+    length_of_slices, positions = calculate_pizza_slices(np_array,rows, columns, minimum, maximum)
+    filename = data_file.replace('in', 'txt')
+    write_result_to_file(length_of_slices, positions, filename)
+
+if __name__ == '__main__':
+    main()
